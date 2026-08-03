9ceee5e91533389ad0f8e78717a2c61ceaf56ff7
ed-kung <edward.kung@gmail.com>
2026-08-03 06:54:11 -0700

    wip tax analysis

--- scripts/tax_analysis.r
+++ scripts/tax_analysis.r
@@ -10,6 +10,8 @@ readRenviron(here(".env"))
 
 ROOT_PATH <- Sys.getenv("ROOT_PATH")
 MY_DATA_PATH <- Sys.getenv("MY_DATA_PATH")
+INPUT_FILEPATH <- paste0(MY_DATA_PATH, "/processed_data/tax_analysis_panel.parquet")
+
 
 # ---- Helper funcs
 
@@ -21,13 +23,13 @@ my_theme <-
         legend.text = element_text(size=12, family="serif", face="plain", color="black"))
 
 # Returns CS DID estimator object
-csdid <- function(yname, gname) {
+csdid <- function(dataframe, yname, gname) {
   o <- att_gt(
     yname = yname,
     gname = gname,
     tname = "year",
     idname = "city_id",
-    data = df,
+    data = dataframe,
     allow_unbalanced_panel = TRUE,
     control_group = "notyettreated"
   )
@@ -51,64 +53,102 @@ es_graph <- function(att_gt_object, title, bw, bal=NULL) {
 
 # ---- Data analysis
 
-INPUT_FILEPATH <- paste0(MY_DATA_PATH, "/tax_analysis_panel.parquet")
-
 df <- read_parquet(INPUT_FILEPATH)
 
+df$A0389 <- df$A03 + df$A89
+
 #df <- filter(df, year<2020)  # remove covid years
-#df <- filter(df, abs(years_from_enforcement)<=12)  # +/- 12 years from enforcement
+#df <- filter(df, year>=2019)  # use Davide time period
+
 
 vars <- c(
   rev_general = "FiSC Revenue (All Sources)",
-  rev_general_city = "City Revenue (All Sources)",
+  #rev_general_city = "City Revenue (All Sources)",
   taxes = "FiSC Tax Revenue (All Tax Sources)",
-  taxes_city = "City Tax Revenue (All Tax Sources)",
-  tax_property = "FiSC Property Tax",
-  tax_property_city = "City Property Tax",
-  tax_sales_general = "FiSC General Sales Tax",
-  tax_sales_general_city = "City General Sales Tax",
-  tax_income = "FiSC Income Tax",
-  tax_income_city = "City Income Tax",
-  tax_license_bus = "FiSC Business and Occupation License Tax",
-  tax_license_bus_city = "City Business and Occupation License Tax",
-  tax_sales_other = "FiSC Selective Sales Tax (Other)",
-  tax_sales_other_city = "City Selective Sales Tax (Other)",
-  charges = "FiSC Revenue from Charge Fees",
-  charges_city = "City Revenue from Charge Fees",
-  chg_other = "FiSC Revenue from Charge Fees (Other)",
-  chg_other_city = "City Revenue from Charge Fees (Other)",
+  #taxes_city = "City Tax Revenue (All Tax Sources)",
+  tax_property = "FiSC Property Tax Revenue",
+  #tax_property_city = "City Property Tax",
+  tax_sales_general = "FiSC General Sales Tax Revenue",
+  #tax_sales_general_city = "City General Sales Tax",
+  tax_income = "FiSC Income Tax Revenue",
+  #tax_income_city = "City Income Tax",
+  #tax_other = "FiSC Tax Revenue (Other)",
+  #tax_other_city = "City Other Tax",
+  tax_sales_selectiv = "FiSC Selective Sales Tax Revenue",
+  tax_licenses = "FiSC License Tax Revenue",
+  tax_transfer = "FiSC Transfer Tax Revenue",
+  #tax_misc = "FiSC Miscellaneous Tax Revenue",
+  #tax_license_bus = "FiSC Business and Occupation License Tax",
+  #tax_license_bus_city = "City Business and Occupation License Tax",
+  #tax_license_other = "FiSC Other License Tax",
+  #tax_license_other_city = "City Other License Tax",
+  #tax_sales_other = "FiSC Selective Sales Tax (Other)",
+  #tax_sales_other_city = "City Selective Sales Tax (Other)",
+  #tax_transfer = "FiSC Transfer Taxes",
+  #tax_transfer_city = "City Transfer Taxes",
+  #misc_fines_fees = "FiSC Fines and Forfeits",
+  #misc_fines_fees_city = "City Fines and Forefeits",
+  charges = "FiSC Charge Revenue",
+  #charges_city = "City Revenue from Charge Fees",
+  #chg_other = "FiSC Revenue from Charge Fees (Other)",
+  #chg_other_city = "City Revenue from Charge Fees (Other)",
+  rev_utility = "FiSC Utility Revenue",
   ZHVI = "City ZHVI"
+  #T29 = "City T29",
+  #A89 = "City A89"
+  #A0389 = "City A03 + A89",
+  #U30 = "City U30"
 )
 
-results <- data.frame(
-  outcome = unname(unlist(vars)),
-  att = NA_real_,
-  se = NA_real_,
-  p = NA_real_
-)
 
-for (i in seq_along(vars)) {
-  col <- names(vars)[i]
-  label <- vars[[i]]
+results <- data.frame()
+
+for (covid in c(TRUE, FALSE)) {
+
+  my_results <- data.frame(
+    outcome = unname(unlist(vars)),
+    att = NA_real_,
+    se = NA_real_,
+    p = NA_real_
+  )
   
-  if (col %in% c("ZHVI")) {
-    df$outcome <- log1p(df[[col]])
+  if (covid) {
+    filename_append <- ""
+    title_append <- ""
   } else {
-    df$outcome <- log1p(df[[col]])  # the data are per capita
+    filename_append <- "_nocovid"
+    title_append <- ", Year<2020"
   }
   
-  o <- csdid("outcome", "enforcement_year")
+  for (i in seq_along(vars)) {
+    col <- names(vars)[i]
+    label <- vars[[i]]
+    
+    if (col %in% c("ZHVI", "A89")) {
+      df$outcome <- log1p(df[[col]]*1000)
+    } else {
+      df$outcome <- log1p(df[[col]])
+    }
+    
+    if (covid) {
+      o <- csdid(df, "outcome", "enforcement_year")
+    } else {
+      o <- csdid(filter(df, year<2020), "outcome", "enforcement_year")
+    }
 
-  r <- aggte(o, type="simple", na.rm=TRUE)
-  results$att[i] <- r$overall.att
-  results$se[i] <- r$overall.se
-  results$p[i] <- 2*pnorm(-abs(r$overall.att / r$overall.se))
-  
-  g <- es_graph(o, label, 6)
-  print(g)
-  filename <- paste0(ROOT_PATH, "/results/csdid_", col, ".png")
-  print(filename)
-  ggsave(filename, plot = g, width=6, height=4)
+    r <- aggte(o, type="simple", na.rm=TRUE)
+    my_results$att[i] <- r$overall.att
+    my_results$se[i] <- r$overall.se
+    my_results$p[i] <- 2*pnorm(-abs(r$overall.att / r$overall.se))
+    
+    g <- es_graph(o, paste0(label, title_append), 6)
+    print(g)
+    filename <- paste0(ROOT_PATH, "/results/csdid_", col, filename_append, ".png")
+    print(filename)
+    ggsave(filename, plot = g, width=6, height=4)
+  }
+  my_results$covid <- covid
+  results <- rbind(results, my_results)
 }
 
 print(results)
